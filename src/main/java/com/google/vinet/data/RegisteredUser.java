fad7a3fd88d5535f8ab14cc4b056bba201a63de1
kevlartest <kevlartest@google.com>
2020-08-31 20:01:50 +0100

    Add volunteer availability servlet

--- src/main/java/com/google/vinet/data/RegisteredUser.java
+++ src/main/java/com/google/vinet/data/RegisteredUser.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2020 Google LLC
+ *  Copyright 2020 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https:www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.google.vinet.data;
@@ -23,6 +23,10 @@ public class RegisteredUser {
 
   public RegisteredUser(String userId) {
     this.userId = Objects.requireNonNull(userId, "User ID must not be null!");
+  }
+
+  public String getUserId() {
+    return userId;
   }
 
   @Override
