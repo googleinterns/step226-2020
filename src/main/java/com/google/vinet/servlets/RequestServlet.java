5cd52ba334a1d809fed81aa61c115b564ef0190b
Thomas O'Gara <thomasogara12@gmail.com>
2020-09-02 11:12:29 +0000

    remove unused imports

--- src/main/java/com/google/vinet/servlets/RequestServlet.java
+++ src/main/java/com/google/vinet/servlets/RequestServlet.java
@@ -21,16 +21,13 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.DatastoreFailureException;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import com.google.vinet.data.Isolate;
 import com.google.vinet.data.IsolateTimeSlot;
 
-import javax.print.attribute.standard.MediaSize.ISO;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
