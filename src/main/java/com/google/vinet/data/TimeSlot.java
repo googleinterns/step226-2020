4e37150837b04f7b46f87b3f769f97dbb26ae33f
kevlartest <kevlartest@google.com>
2020-09-02 14:47:56 +0100

    Add time slot overlap method

--- src/main/java/com/google/vinet/data/TimeSlot.java
+++ src/main/java/com/google/vinet/data/TimeSlot.java
@@ -1,22 +1,23 @@
 /*
- * Copyright 2020 Google LLC
+ *  Copyright 2020 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https:www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.google.vinet.data;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
@@ -99,6 +100,15 @@ public abstract class TimeSlot implements Comparable<TimeSlot> {
    */
   public boolean contains(TimeSlot timeSlot) {
     return compareTo(timeSlot) <= 0 && TimeSlotEndComparator.compare(this, timeSlot) >= 0;
+  }
+
+  public boolean hasMinimumOverlap(TimeSlot timeSlot, long minMinutesOverlap) {
+    if (start.isBefore(timeSlot.start)) {
+      // If difference is negative, they don't overlap, and will return false.
+      return ChronoUnit.MINUTES.between(timeSlot.start, end) > minMinutesOverlap;
+    } else {
+      return ChronoUnit.MINUTES.between(start, timeSlot.end) > minMinutesOverlap;
+    }
   }
 
   @Override
