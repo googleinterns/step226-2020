457e96581a3b7f6452c49ef8db0ca26c9f71347c
kevlartest <kevlartest@google.com>
2020-09-02 11:16:12 +0100

    Add error message for user not logged in for registration servlet

--- src/main/java/com/google/vinet/servlets/RegistrationServlet.java
+++ src/main/java/com/google/vinet/servlets/RegistrationServlet.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2020 Google LLC
+ *  Copyright 2020 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https:www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.google.vinet.servlets;
@@ -69,6 +69,7 @@ public class RegistrationServlet extends HttpServlet {
     if (request == null) return;
 
     if (!userService.isUserLoggedIn()) {
+      System.err.println("User is not logged in!");
       response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
       return;
     }
@@ -81,6 +82,7 @@ public class RegistrationServlet extends HttpServlet {
         return;
       }
 
+      // Setup variables before verifying and validating input
       UserType userType = null;
       final Set<String> propertyNames =
               Sets.newHashSet("firstname", "lastname", "type", "latitude", "longitude");
