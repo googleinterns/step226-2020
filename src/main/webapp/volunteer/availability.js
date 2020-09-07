515be37cf4be7dcfe63b0f1b7cd26c6f4ed2ae17
kevlartest <kevlartest@google.com>
2020-09-07 11:08:08 +0100

    Document availability Javascript functions

--- src/main/webapp/volunteer/availability.js
+++ src/main/webapp/volunteer/availability.js
@@ -14,34 +14,48 @@
  *  limitations under the License.
  */
 
+/**
+ * Fills the webpage with the existing timeslots etc.
+ */
 function initialise() {
     populateRows();
 }
 
+/**
+ * Inserts existing timeslots in webpage.
+ */
 function populateRows() {
-    getExistingTimeSlots().then(rows => {
-        rows.forEach(row => addRow(row))
-    });
+    getExistingTimeSlots().then(rows => rows.forEach(row => addRow(row)));
 
     //TODO add delete button to each row
 
     //TODO validate fields so they're not empty, and start time is before end time
 }
-//TODO to fix: weird bug where re-fetching slots, the start loses 1 hour???
+
+/**
+ * Fetches the list of existing timeslots.
+ * @returns {Promise<Array>} Array of timeslot objects.
+ */
 async function getExistingTimeSlots() {
     const slots = await (await fetch('/volunteer-availability')).json();
     const rows = slots.map(slot => slotToRow(slot));
+
     // Sort timeslots by start time
     return [...rows].sort((a, b) => (new Date(a.children["ISO-start-time"].value)).getTime() >
     (new Date(b.children["ISO-start-time"].value)).getTime() ? 1 : -1);
 }
 
+/**
+ * Convert slot element to row div for insertion intro form.
+ * @param slot The slot element to convert.
+ * @returns {HTMLDivElement} The row containing the slot data.
+ */
 function slotToRow(slot) {
-    //parse start.seconds and end.seconds, getting the date and the day in the local timezone
+    // Get start and end instant and display as local timezone.
     const startTime = new Date(slot.start.seconds * 1000);
     const endTime = new Date(slot.end.seconds * 1000);
 
-    // Get local time in HH:MM format
+    // Get local time in HH:MM format.
     const hours = String(endTime.getHours()).padStart(2, "0");
     const minutes = String(endTime.getMinutes()).padStart(2, "0");
     const end = hours + ":" + minutes;
@@ -49,16 +63,25 @@ function slotToRow(slot) {
     return constructNewRow(startTime.getTime(), end);
 }
 
+/**
+ * Set value of hidden ISO start time element by parsing start date and time.
+ * @param {HTMLInputElement} inputElement The start datetime element.
+ */
 function fillISOStartTime(inputElement) {
     if (inputElement.value == null || inputElement.value === "") return;
     inputElement.parentNode.children["ISO-start-time"].value = new Date(inputElement.value).toISOString();
     //TODO also update end time accordingly to date change
-    //TODO resort this slot in list after changing?
+    //TODO re-sort this slot in list after changing?
 }
 
+/**
+ * Set value of hidden ISO end time element by parsing start date and end time.
+ * @param {HTMLInputElement} inputElement The end time element.
+ */
 function fillISOEndTime(inputElement) {
     const children = inputElement.parentNode.children;
     const endValue = inputElement.value;
+
     // Get the date from the start datetime object
     const date = new Date(children["ISO-start-time"].value);
 
@@ -71,6 +94,12 @@ function fillISOEndTime(inputElement) {
     children["ISO-end-time"].value = date.toISOString();
 }
 
+/**
+ * Creates a new row element with the timeslots details.
+ * @param {String} startTimeValue The value for datetime-local start date and time.
+ * @param {String} endTimeValue The value for local end time.
+ * @returns {HTMLDivElement} The div containing the row.
+ */
 function constructNewRow(startTimeValue, endTimeValue) {
     const newRow = document.createElement("div");
 
@@ -103,15 +132,23 @@ function constructNewRow(startTimeValue, endTimeValue) {
     newRow.appendChild(ISOStartTime);
     newRow.appendChild(ISOEndTime);
 
-    fillISOStartTime(startTime); // initialise with a value for submission
-    fillISOEndTime(endTime); // initialise with a value for submission
+    // Initialise with fetches values, if any
+    fillISOStartTime(startTime);
+    fillISOEndTime(endTime);
     return newRow;
 }
 
+/**
+ * Add a row to the submission form.
+ * @param {HTMLDivElement} row The row to add.
+ */
 function addRow(row) {
     document.getElementById("availability-form").appendChild(row);
 }
 
+/**
+ * Insert an empty row in the submission form.
+ */
 function addEmptyRow() {
     addRow(constructNewRow());
 }
