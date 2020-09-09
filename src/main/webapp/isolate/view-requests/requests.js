726072b96aa91834341e2a1465d137d90e179137
Thomas O'Gara <thomasogara12@gmail.com>
2020-09-09 16:57:22 +0000

    update request ticket styling to match form

--- src/main/webapp/isolate/view-requests/requests.js
+++ src/main/webapp/isolate/view-requests/requests.js
@@ -6,46 +6,47 @@ window.onload = async () => {
 
   console.log(requests);
 
-  for (let request of requests) {
-    const row = document.createElement('tr');
+  for(let request of requests) {
+  const row = document.createElement('tr');
 
-    const date = document.createElement('td');
-    const start = document.createElement('td');
-    const end = document.createElement('td');
+  const date = document.createElement('td');
+  const start = document.createElement('td');
+  const end = document.createElement('td');
 
-    const tickets = document.createElement('td');
+  const tickets = document.createElement('td');
 
-    date.innerText = request.date;
-    start.innerText = request.start;
-    end.innerText = request.end;
+  date.innerText = request.date;
+  start.innerText = request.start;
+  end.innerText = request.end;
 
-    /* Iterate through the tickets for a request, and add them to the table. */
-    for(let i = 0; i < request.subjects.length; i++) {
-      const ticket = document.createElement('div');
-      const ticketBody = document.createElement('div');
-      const detail = document.createElement('h5');
-      const subject = document.createElement('p');
+  /* Iterate through the tickets for a request, and add them to the table. */
+  for (let i = 0; i < request.subjects.length; i++) {
+    const ticket = document.createElement('div');
+    const ticketBody = document.createElement('div');
+    const subject = document.createElement('p');
+    const detail = document.createElement('h3');
 
-      ticket.classList.add('card');
-      ticketBody.classList.add('card-body');
-      details.classList.add('card-title');
-      subjects.classList.add('card-text');
 
-      ticket.appendChild(ticketBody);
-      ticketBody.appendChild(detail);
-      ticketBody.appendChild(subject);
+    ticket.classList.add('card');
+    ticketBody.classList.add('card-body');
+    subject.classList.add('card-title');
+    detail.classList.add('card-text');
 
-      detail.innerText = request.details[i];
-      subject.innerText = request.subjects[i];
+    ticket.appendChild(ticketBody);
+    ticketBody.appendChild(subject);
+    ticketBody.appendChild(detail);
 
-      tickets.appendChild(ticket);
-    }
+    detail.innerText = request.details[i];
+    subject.innerText = request.subjects[i];
 
-    row.appendChild(date);
-    row.appendChild(start);
-    row.appendChild(end);
-    row.appendChild(ticket);
+    tickets.appendChild(ticket);
+  }
 
-    table.appendChild(row);
+  row.appendChild(date);
+  row.appendChild(start);
+  row.appendChild(end);
+  row.appendChild(tickets);
+
+  table.appendChild(row);
   }
 }
