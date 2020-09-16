a4ec3023c8ce7e8e2074b003b8489a3b082a46ca
Thomas O'Gara <thomasogara12@gmail.com>
2020-09-16 18:09:16 +0100

    remove unused doGet in RequestServlet

--- src/main/java/com/google/vinet/servlets/RequestServlet.java
+++ src/main/java/com/google/vinet/servlets/RequestServlet.java
@@ -227,46 +227,6 @@ public class RequestServlet extends HttpServlet {
     response.sendRedirect("/isolate/home.html");
   }
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-    if (response == null) {
-      throw new IllegalArgumentException("response must not be null");
-    }
-
-    if (request == null) {
-      throw new IllegalArgumentException("request must not be null");
-    }
-
-    if (!this.userService.isUserLoggedIn()) {
-      response.sendError(
-          HttpServletResponse.SC_UNAUTHORIZED,
-          "user must be logged in to fetch a request"
-      );
-      return;
-    }
-
-    final boolean registered = registrationServlet.isUserRegistered(this.userService);
-
-    if (!registered) {
-      response.sendError(
-          HttpServletResponse.SC_UNAUTHORIZED,
-          "user must be registered to fetch a request"
-      );
-      return;
-    }
-
-    IsolateTimeSlot.datastore = this.datastore;
-    final List<IsolateTimeSlot> timeSlots = IsolateTimeSlot.getTimeslotsByUserId(this.userService.getCurrentUser().getUserId());
-
-    try{
-      Gson gson = new GsonBuilder().setPrettyPrinting().create();
-      response.getWriter().println(gson.toJson(timeSlots));
-    } catch (Exception exception) {
-      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      throw exception;
-    }
-  }
-
   /**
    * Apply the trim() method to all non-null members of a String array,
    * in-place.
