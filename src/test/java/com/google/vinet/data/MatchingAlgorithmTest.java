b44727dc33eb1400e0637867c87ed413f0f9ce39
kevlartest <kevlartest@google.com>
2020-09-18 16:33:46 +0100

    Add more conflicting matching timeslots tests

--- src/test/java/com/google/vinet/data/MatchingAlgorithmTest.java
+++ src/test/java/com/google/vinet/data/MatchingAlgorithmTest.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2020 Google LLC
+ *  Copyright 2020 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *      https:www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.google.vinet.data;
@@ -19,11 +19,11 @@ package com.google.vinet.data;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.util.HashSet;
 import java.util.Set;
-import java.time.Clock;
 
 import static java.time.temporal.ChronoUnit.HOURS;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -223,5 +223,29 @@ public class MatchingAlgorithmTest {
     Set<IsolateTimeSlot> matched =
             MatchingAlgorithm.matchTimeSlots(isolateTimeSlots, volunteerTimeSlots);
     assert (matched.size() == 0);
+  }
+
+  @Test
+  public void testNotEnoughVolunteers() {
+    isolateTimeSlots.add(new IsolateTimeSlot(now, now.plus(1, HOURS), null));
+    isolateTimeSlots.add(new IsolateTimeSlot(now, now.plus(1, HOURS), new Isolate("a")));
+    isolateTimeSlots.add(new IsolateTimeSlot(now, now.plus(1, HOURS), new Isolate("b")));
+
+    volunteerTimeSlots.add(new VolunteerTimeSlot(now, now.plus(1, HOURS), null));
+    volunteerTimeSlots.add(new VolunteerTimeSlot(now, now.plus(1, HOURS), new Volunteer("c")));
+
+    assert (MatchingAlgorithm.matchTimeSlots(isolateTimeSlots, volunteerTimeSlots).size() == 2);
+  }
+
+  @Test
+  public void testNotEnoughIsolates() {
+    isolateTimeSlots.add(new IsolateTimeSlot(now, now.plus(1, HOURS), null));
+    isolateTimeSlots.add(new IsolateTimeSlot(now, now.plus(1, HOURS), new Isolate("a")));
+
+    volunteerTimeSlots.add(new VolunteerTimeSlot(now, now.plus(1, HOURS), null));
+    volunteerTimeSlots.add(new VolunteerTimeSlot(now, now.plus(1, HOURS), new Volunteer("b")));
+    volunteerTimeSlots.add(new VolunteerTimeSlot(now, now.plus(1, HOURS), new Volunteer("c")));
+
+    assert (MatchingAlgorithm.matchTimeSlots(isolateTimeSlots, volunteerTimeSlots).size() == 2);
   }
 }
