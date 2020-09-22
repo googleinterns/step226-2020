3c91c46e174a26d1f182f34f282bdf538abf4880
Thomas O'Gara <thomasogara12@gmail.com>
2020-09-22 16:25:38 +0100

    remove second getUserId() declaration

--- src/main/java/com/google/vinet/data/RegisteredUser.java
+++ src/main/java/com/google/vinet/data/RegisteredUser.java
@@ -27,10 +27,6 @@ public class RegisteredUser {
     return userId;
   }
 
-  public String getUserId() {
-    return userId;
-  }
-
   @Override
   public int hashCode() {
     return userId.hashCode();
